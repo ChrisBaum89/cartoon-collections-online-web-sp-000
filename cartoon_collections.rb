486d48b7ab6cd4571cd16584b7549b58b7d89057
Chris Baum <christopher.baum@valpo.edu>
2021-01-13 01:13:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -31,7 +31,7 @@ def find_the_cheese(list)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   list.first do |i|
-    i == cheese_types[1] || i == cheese_types[2] || i == cheese_types[3]
+    i == cheese_types[0] || i == cheese_types[1] || i == cheese_types[2]
   end
 end
 puts "#{find_the_cheese(list)}"
