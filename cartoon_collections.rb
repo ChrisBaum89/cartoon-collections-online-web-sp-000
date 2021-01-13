35f10ccf7544d3de138e485e60285d6b8e0bb43f
Chris Baum <christopher.baum@valpo.edu>
2021-01-13 01:00:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,16 +6,12 @@ def roll_call_dwarves(names)
   end
 end
 
-#fruits = %w[apple banana orange]
-#puts "#{fruits}"
 def summon_captain_planet(words)
   new_array = Array.new
   words.collect do |word|
     new_array = "#{word.capitalize}!"
   end
-  #puts "#{new_array}"
 end
-#puts "#{summon_captain_planet(fruits)}"
 
 def long_planeteer_calls# code an argument here
   # Your code here
